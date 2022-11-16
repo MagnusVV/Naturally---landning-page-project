8fe663f153f42813cab3ce21a5b54256f82417af
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2022-11-16 17:07:37 +0100

    hamburger.menu

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".navlink").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
